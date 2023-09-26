6a6a7fa39f01c62c426e2c2e605feed8a0384a11
lukasanader <lukasanader@hotmail.com>
2023-09-26 13:25:02 +0100

    work

--- primes.py
+++ primes.py
@@ -3,4 +3,28 @@
 
 def primes(number_of_primes):
     list = []
+    i=0
+    primenumber=1
+    prime =False
+    if(number_of_primes>0):
+        while (i<number_of_primes):
+            for x in range(2,int(primenumber/2)+2):
+                if(primenumber%x!=0):
+                    prime=True
+                else:
+                    if(primenumber==x):
+                        prime=True
+                        break
+                    prime=False
+                    break
+                
+            if(prime):
+                list.append(primenumber)
+                i=i+1
+            primenumber=primenumber+1
+    else:
+        raise ValueError
+
     return list
+
+print(primes(10))
